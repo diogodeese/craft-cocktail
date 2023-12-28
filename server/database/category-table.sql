a54959841aaa67d903ec0e9ff48d434ba26ec56f
Diogo Santos <diogodeese@gmail.com>
2023-12-28 22:46:06 +0000

    chore: create recipe, user and category table

---
+++ server/database/category-table.sql
@@ -0,0 +1,6 @@
+use craftcocktail;
+
+CREATE TABLE category (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
---
+++ server/database/recipe-table.sql
@@ -0,0 +1,14 @@
+use craftcocktail;
+
+CREATE TABLE receip (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    recipe_name VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    ingredients TEXT NOT NULL,
+    preparation_description TEXT NOT NULL,
+    difficulty INT CHECK (difficulty BETWEEN 1 AND 5),
+    category_id INT,
+    time TIME NOT NULL,
+    cost VARCHAR(50) NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
