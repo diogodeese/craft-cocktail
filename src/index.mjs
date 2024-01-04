fb6f99a66b78aae0f07cb06ca860a6342971812b
Diogo Santos <diogodeese@gmail.com>
2024-01-04 19:44:38 +0000

    feat: mustache engine

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,8 @@
 import cors from 'cors'
 import express from 'express'
+import mustacheExpress from 'mustache-express'
 import mysql2 from 'mysql2'
+import { default as __dirname, default as path } from 'path'
 import { router } from './routes.mjs'
 
 const app = express()
@@ -18,6 +20,12 @@ app.use(
     origin: '*',
   })
 )
+
+// Mustache engine
+app.engine('mustache', mustacheExpress())
+app.set('view engine', 'mustache') // extensão dos ficheiros das views
+app.set('views', path.join(__dirname.resolve(), 'src/views')) // indicação de qual a pasta que irá conter as views
+
 app.use(express.json())
 app.use(router)
 
