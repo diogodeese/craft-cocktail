10e2040097ce9fda224e9ca990866462c371e2b9
Diogo Santos <diogodeese@gmail.com>
2024-01-04 19:53:15 +0000

    fix: use getConnection function

--- src/index.mjs
+++ src/index.mjs
@@ -1,19 +1,14 @@
 import cors from 'cors'
 import express from 'express'
 import mustacheExpress from 'mustache-express'
-import mysql2 from 'mysql2'
 import { default as __dirname, default as path } from 'path'
+import { getConnection } from './config/database-connection.mjs'
 import { router } from './routes.mjs'
 
 const app = express()
 const port = 3001
 
-const connection = mysql2.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'mysqldiogo8660', // Your Password
-  database: 'craftcocktail',
-})
+const connection = getConnection()
 
 app.use(
   cors({
@@ -30,10 +25,5 @@ app.use(express.json())
 app.use(router)
 
 app.listen(port, () => {
-  connection.connect((err) => {
-    if (err) throw err
-    console.log('Connected to the database.')
-  })
-
   console.log(`Example app listening on port ${port}`)
 })
