5a44688ca93de7ab37c2305693eea25963098221
Diogo Santos <diogodeese@gmail.com>
2024-01-05 20:59:48 +0000

    chore: remove console log

--- src/controllers/recipe/get.mjs
+++ src/controllers/recipe/get.mjs
@@ -4,6 +4,6 @@ export const get = async (request, response) => {
   const { id } = request.params
 
   const recipe = await recipeService.get(id)
-  console.log(recipe)
+
   response.render('recipe', recipe)
 }
