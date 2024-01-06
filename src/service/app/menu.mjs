677fb0230b1350b81f4c4055ba68661b93c46c3d
Diogo Santos <diogodeese@gmail.com>
2024-01-06 19:49:25 +0000

    chore: remove logs

--- src/service/app/menu.mjs
+++ src/service/app/menu.mjs
@@ -9,11 +9,10 @@ export const menu = async () => {
     const query = 'SELECT * FROM menu ORDER BY `order` ASC'
     const [rows] = await connection.execute(query)
 
-    console.log(rows)
     return rows
   } catch (error) {
     console.error('Error fetching menu items:', error)
-    throw error // Rethrow the error for handling in the calling code
+    throw error
   } finally {
     if (connection) {
       connection.release()
