4d712aef23e3e8b65f72292743aef01dedd3505e
Kenny-Dev <66855201+0xkenny@users.noreply.github.com>
2024-01-15 04:33:05 +0000

    feat: added filter recipe by category

--- src/controllers/recipe/get-by-category.mjs
+++ src/controllers/recipe/get-by-category.mjs
@@ -3,7 +3,7 @@ import recipeService from '../../service/recipe/index.mjs'
 export const getByCategory = async (request, response) => {
   const { categoryId } = request.params
 
-  const categories = await recipeService.getByCategory(categoryId)
+  const recipeByCategory = await recipeService.getByCategory(categoryId)
 
-  response.json({ categories })
+  response.render('recipescategory', { recipeByCategory })
 }
