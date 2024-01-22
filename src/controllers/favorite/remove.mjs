45bb1e89b5e542699d6654a65d82dbd88ef3d33a
Kenny-Dev <66855201+0xkenny@users.noreply.github.com>
2024-01-22 02:03:07 +0000

    chore: update remove to get recipeId instead of favoriteId

--- src/controllers/favorite/remove.mjs
+++ src/controllers/favorite/remove.mjs
@@ -1,13 +1,24 @@
 import favoriteService from '../../service/favorite/index.mjs'
+import jwt from 'jsonwebtoken'
 
 export const remove = async (request, response) => {
-  const { favoriteId } = request.params
+  const token = request.headers.authorization?.split(' ')[1]
+  const { recipeId } = request.body
 
-  if (!favoriteId) {
-    return response.status(400).json({ error: 'Error trying to unfavorite a recipe' })
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    request.userId = decoded.userId
+  } catch (error) {
+    console.error('Unauthenticated: ' + error)
   }
 
-  let result = await favoriteService.remove(favoriteId)
+  if (!recipeId) {
+    return response
+      .status(400)
+      .json({ error: 'Error trying to unfavorite a recipe' })
+  }
+
+  let result = await favoriteService.remove(recipeId, request.userId)
 
   response.json({ result: result })
 }
