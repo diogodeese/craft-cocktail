cf9d8680a0ceacab0de403c3b6ba7d0f561002be
Kenny-Dev <66855201+0xkenny@users.noreply.github.com>
2024-01-25 03:47:23 +0000

    chore: update to have category_name in recipe info view

--- src/service/recipe/get.mjs
+++ src/service/recipe/get.mjs
@@ -6,7 +6,11 @@ export const get = async (id) => {
   try {
     connection = await getConnection()
 
-    const query = `SELECT * FROM recipe WHERE id = ${id}`
+    const query = `SELECT recipe.*, category.name AS category_name 
+                  FROM recipe
+                  JOIN category 
+                  ON recipe.category_id = category.id
+                  WHERE recipe.id = ${id};`
     const [rows] = await connection.execute(query)
 
     return rows.length > 0 ? rows[0] : null
